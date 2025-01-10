214f246297359bb8488a23ccb882a717a50353e6
Badnam xd <yadavxd746@gmail.com>
2025-01-10 15:20:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -85,13 +85,13 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/ltu4no.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/ltu4no.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/nef735.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/ltu4no.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/nef735.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/nef735.jpg"
 SOUNCLOUD_IMG_URL = "https://files.catbox.moe/nef735.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/ltu4no.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
