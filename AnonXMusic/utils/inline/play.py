d823e9a252ad3f5653064de3289384b1650e8fe9
Badnam xd <yadavxd746@gmail.com>
2025-01-16 21:03:07 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -52,26 +52,19 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "————————◉—"
     else:
         bar = "—————————◉"
-    elif
     buttons = [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],
-        [
-            InlineKeyboardButton(
-                text="˹❍ᴡηєʀ", url="https://t.me/FRIEND_X_ZONE_CHATING_GROUP"
-            ),
-            InlineKeyboardButton(
-                text="• ɢʀᴏᴜᴘ •", url="https://t.me/FRIEND_X_ZONE_CHATING_GROUP"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="˹ ᴧʟʟ ʙσᴛ's  ", url="https://t.me/FRIEND_X_ZONE_CHATING_GROUP"
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -86,16 +79,6 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text="˹❍ᴡηєʀ", url="https://t.me/FRIEND_X_ZONE_CHATING_GROUP",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="• ɢʀᴏᴜᴘ • ", url="https://t.me/FRIEND_X_ZONE_CHATING_GROUP",
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
